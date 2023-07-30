0667d9ea03780032a1510ae53cf28d2834d56ec4
Eric Cheung <49351150+eric1162004@users.noreply.github.com>
2023-07-30 01:15:40 -0700

    Update node.pp

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,7 +1,9 @@
 node 'pc2.localdomain' {
   include java::install
   include tomcat
-  include chrony
+  class { 'chrony':
+    servers => [ '0.rhel1.pool.ntp.org', '1.thedevopscloud.pool.ntp.org'],
+  }
 }
 node 'pc3.localdomain' {
 
